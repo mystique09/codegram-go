38de331e2a71c36f1dc4a5bdd7e73de6f163c944
mystique09 <knightkrypto7@gmail.com>
2022-02-13 11:46:49 +0800

    add post route

---
+++ routes/post.go
@@ -0,0 +1,70 @@
+package routes
+
+import (
+	"codegram/db"
+	"codegram/ent"
+	"context"
+	"net/http"
+
+	"github.com/google/uuid"
+	"github.com/labstack/echo/v4"
+)
+
+type (
+	PostRoute struct {
+		Client *ent.Client
+	}
+)
+
+func (post_rt *PostRoute) GetAllPost(c echo.Context) error {
+	p, err := db.QueryPosts(context.Background(), post_rt.Client)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, NewError(err.Error()))
+	}
+	return c.JSON(http.StatusOK, NewResponse(true, "Successfully queried all post.", p))
+}
+
+func (post_rt *PostRoute) GetPostById(c echo.Context) error {
+	puid, uuid_err := uuid.Parse(c.Param("id"))
+
+	if uuid_err != nil {
+		return c.JSON(http.StatusBadRequest, NewError(uuid_err.Error()))
+	}
+
+	p, err := db.QueryPost(context.Background(), post_rt.Client, puid)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, NewError(err.Error()))
+	}
+	return c.JSON(http.StatusOK, NewResponse(true, "Successfully queried one post.", p))
+}
+
+func (post_rt *PostRoute) CreatePost(c echo.Context) error {
+	auid, uuid_err := uuid.Parse(c.Param("id"))
+
+	if uuid_err != nil {
+		return c.JSON(http.StatusBadRequest, NewError(uuid_err.Error()))
+	}
+
+	payload := new(db.CPost)
+
+	if err := (&echo.DefaultBinder{}).BindBody(c, &payload); err != nil {
+		return c.JSON(http.StatusBadRequest, NewError(err.Error()))
+	}
+
+	p, err := db.CreatePost(context.Background(), post_rt.Client, auid, *payload)
+
+	if err != nil {
+		return c.JSON(http.StatusBadRequest, NewError(err.Error()))
+	}
+	return c.JSON(http.StatusCreated, NewResponse(true, "New post created.", p))
+}
+
+func (post_rt *PostRoute) UpdatePost(c echo.Context) error {
+
+}
+
+func (post_rt *PostRoute) DeletePost(c echo.Context) error {
+
+}
