775018b5415a119f478729a9980905bfd71b6d8b
mystique09 <knightkrypto7@gmail.com>
2022-02-13 12:14:09 +0800

    fix types in .env file

--- db/setup.go
+++ db/setup.go
@@ -5,14 +5,15 @@ import (
 	"context"
 	"database/sql"
 	"log"
+	"os"
 
 	"entgo.io/ent/dialect"
 	entsql "entgo.io/ent/dialect/sql"
 	_ "github.com/jackc/pgx/v4/stdlib"
+	"github.com/joho/godotenv"
 )
 
 func Open(db_url string) *ent.Client {
-
 	db, err := sql.Open("pgx", db_url)
 
 	if err != nil {
@@ -24,7 +25,15 @@ func Open(db_url string) *ent.Client {
 }
 
 func SetupDb() *ent.Client {
-	client := Open("postgres://mystique09:mystique09@localhost/codegram")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("error loading env file, maybe missing ?")
+	}
+
+	db_url := os.Getenv("DB_URL")
+
+	client := Open(db_url)
 	ctx := context.Background()
 
 	if err := client.Schema.Create(ctx); err != nil {
